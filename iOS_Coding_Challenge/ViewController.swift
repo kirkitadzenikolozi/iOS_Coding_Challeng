aed752608d593261b86503c63152f426a6b5f1a4
Nika Kirkitadze <kirkitadzenikolozi@gmail.com>
2019-02-23 15:01:55 +0400

    Initial Commit

---
+++ iOS_Coding_Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Coding_Challenge
+//
+//  Created by Nika Kirkitadze on 2/23/19.
+//  Copyright © 2019 organization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
